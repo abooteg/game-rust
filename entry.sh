dd57a5fe495670e6340f795762dce7c71be31750
booteg <abooteg@gmail.com>
2021-12-28 12:18:32 +0300

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -9,7 +9,7 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
 # Believe it or not, if you don't do this srcds_run shits itself
 cd "${STEAMAPPDIR}"
 
-bash "./RustDedicated" -batchmode -nographics -keeplog +server.ip "${RUST_SERVER_IP}" \ 
+bash "./RustDedicated -batchmode -nographics -keeplog +server.ip "${RUST_SERVER_IP}" \ 
             +server.port "${RUST_SERVER_PORT}" +app.listenip "${RUST_APP_IP}" +app.publicip "${RUST_APP_PUBLIC_IP}" +rcon.ip "${RUST_RCON_IP}" \ 
 			+rcon.port "${RUST_RCON_PORT}" +rcon.password "${RUST_RCON_PASSWORD}" +oxide.directory "server/survivalhost.org/oxide" \ 
 			+server.radiation "${RUST_SERVER_RADIATION}" +server.hostname "${RUST_SERVER_HOSTNAME}" +server.identity "survivalhost.org" \ 
@@ -19,4 +19,4 @@ bash "./RustDedicated" -batchmode -nographics -keeplog +server.ip "${RUST_SERVER
 			+server.salt "${RUST_SERVER_SALT}" +server.encryption ${RUST_SERVER_ENCRYPTION} \ 
 			+craft.instant ${RUST_CRAFT_INSTANT} ${RUST_LEVELURL} ${RUST_MAPURL} -logfile ${STEAMAPPDIR}\server\survivalhost.org\server_log\console.log \ 
 			+server.tags "${RUST_SERVER_TAGS}" +server.gamemode "${RUST_SERVER_GAMEMODE}" +server.description "${RUST_SERVER_DESCRIPTION}" \
-			"${ADDITIONAL_ARGS}"
+			"${ADDITIONAL_ARGS}""
