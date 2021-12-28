43a58e4549fdf486ce1936b3bde050233ad07013
booteg <abooteg@gmail.com>
2021-12-28 15:17:39 +0300

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -9,18 +9,15 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
 # Believe it or not, if you don't do this srcds_run shits itself
 cd "${STEAMAPPDIR}"
 
-"./RustDedicated" -batchmode -nographics -keeplog +server.ip ${RUST_SERVER_IP} \
- +server.port ${RUST_SERVER_PORT} +app.listenip ${RUST_APP_IP} +app.publicip ${RUST_APP_PUBLIC_IP} \
-  +rcon.ip ${RUST_RCON_IP} +rcon.port ${RUST_RCON_PORT} +rcon.password ${RUST_RCON_PASSWORD} \
-   +oxide.directory "server/survivalhost.org/oxide" +server.radiation ${RUST_SERVER_RADIATION} \
-    +server.hostname \\\"${RUST_SERVER_HOSTNAME}\\\" +server.identity "survivalhost.org" +server.level \\\"${RUST_SERVER_LEVEL}\\\" \
-	 +server.seed ${RUST_SERVER_SEED} +server.maxplayers ${RUST_SERVER_MAXPLAYERS} +fps.limit ${RUST_FPS_LIMIT} \
-	  +server.worldsize ${RUST_SERVER_WORLDSIZE} +server.saveinterval ${RUST_SERVER_SAVEINTERVAL} \
-	   +server.secure ${RUST_SERVER_SECURE} +server.url ${RUST_SERVER_URL} +server.headerimage ${RUST_SERVER_HEADERIMAGE} \
-	    +server.salt ${RUST_SERVER_SALT} +server.encryption ${RUST_SERVER_ENCRYPTION} \
-		 +craft.instant ${RUST_CRAFT_INSTANT} ${RUST_LEVELURL} ${RUST_MAPURL} \
-		 +server.tags ${RUST_SERVER_TAGS} \
-		   +server.gamemode ${RUST_SERVER_GAMEMODE} +server.description \\\"${RUST_SERVER_DESCRIPTION}\\\" ${ADDITIONAL_ARGS}
-
-
-\\\"{{HOSTNAME}}\\\"
+"./RustDedicated" -batchmode -nographics -keeplog +server.ip "${RUST_SERVER_IP}" \
+ +server.port "${RUST_SERVER_PORT}" +app.listenip "${RUST_APP_IP}" +app.publicip "${RUST_APP_PUBLIC_IP}" \
+  +rcon.ip "${RUST_RCON_IP}" +rcon.port "${RUST_RCON_PORT}" +rcon.password "${RUST_RCON_PASSWORD}" \
+   +oxide.directory "server/survivalhost.org/oxide" +server.radiation "${RUST_SERVER_RADIATION}" \
+    +server.hostname "${RUST_SERVER_HOSTNAME}" +server.identity "survivalhost.org" +server.level "${RUST_SERVER_LEVEL}" \
+	 +server.seed "${RUST_SERVER_SEED}" +server.maxplayers "${RUST_SERVER_MAXPLAYERS}" +fps.limit "${RUST_FPS_LIMIT}" \
+	  +server.worldsize "${RUST_SERVER_WORLDSIZE}" +server.saveinterval "${RUST_SERVER_SAVEINTERVAL}" \
+	   +server.secure "${RUST_SERVER_SECURE}" +server.url "${RUST_SERVER_URL}" +server.headerimage "${RUST_SERVER_HEADERIMAGE}" \
+	    +server.salt "${RUST_SERVER_SALT}" +server.encryption "${RUST_SERVER_ENCRYPTION}" \
+		 +craft.instant "${RUST_CRAFT_INSTANT}" "${RUST_LEVELURL}" "${RUST_MAPURL}" \
+		 +server.tags "${RUST_SERVER_TAGS}" \
+		   +server.gamemode "${RUST_SERVER_GAMEMODE}" +server.description "${RUST_SERVER_DESCRIPTION}" "${ADDITIONAL_ARGS}"
